7adf3990c1e6f3f166a01327a66d3bfa5ec348f8
Saisrinivas122 <112487895+Saisrinivas122@users.noreply.github.com>
2022-09-15 09:16:48 +0530

    Update and rename removing a character in a word.py to removing a character in a word using function.py

---
+++ removing a character in a word using function.py
@@ -0,0 +1,11 @@
+def delchar (s,c):
+    if len(c) == 1:
+        for i in range(len(s)):
+            if c not in s[i]:
+                print(s[i])
+            else:
+                continue
+string = input("enter the string:") 
+char = input("enter the character that is to be removed:")            
+delchar(string,char)        
+         
